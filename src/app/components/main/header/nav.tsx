239d00d780972cac6a47a350f6539562b3c82173
vikei <vikei.dev@gmail.com>
2020-05-24 18:26:37 +0300

    Add auth navigation

--- src/app/components/main/header/nav.tsx
+++ src/app/components/main/header/nav.tsx
@@ -1,17 +1,28 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useAuth } from "../../../../auth"
 import { NavWrapper } from "./styles"
 
 const Nav = () => {
+  const { isAuth } = useAuth()
+
   return (
     <NavWrapper>
       <ul>
-        <li>
-          <Link to={"/registration"}>Sign Up</Link>
-        </li>
-        <li>
-          <Link to={"/login"}>Login</Link>
-        </li>
+        {isAuth ? (
+          <li>
+            <Link to={"/projects"}>Projects</Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to={"/registration"}>Sign Up</Link>
+            </li>
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </NavWrapper>
   )
